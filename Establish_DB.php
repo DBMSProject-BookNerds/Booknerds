748f890726ae8692165cfcf51f498ec1f6b498a6
kaufmantw <kaufmantw@jacks.sfasu.edu>
2023-12-02 16:00:09 -0600

    changed table query syntax
    added auto_incrementing to primary keys

--- Establish_DB.php
+++ Establish_DB.php
@@ -1,5 +1,7 @@
 <?php
     function create_database(){
+        //boolean for whether or not to create data.
+        $create_data = false;
         
         // establish the database
         // change permissions as needed
@@ -27,16 +29,17 @@
 
             // create the tables
             $table = array("CREATE TABLE Bookstore
-                            (bookstoreID int PRIMARY KEY,
+                            (bookstoreID int NOT NULL AUTO_INCREMENT,
                             bookstoreName varchar(50),
                             city varchar(50),
                             stateName varchar(50),
                             postalCode int,
                             addressName varchar(50),
-                            phone varchar(20))",
+                            phone varchar(20),
+                            PRIMARY KEY(bookstoreID))",
 
                             "CREATE TABLE Customer
-                            (customerID int PRIMARY KEY,
+                            (customerID int NOT NULL AUTO_INCREMENT,
                             firstName varchar(20),
                             lastName varchar(20),
                             email varchar(50),
@@ -44,27 +47,31 @@
                             addressName varchar(50),
                             city varchar(50),
                             stateName varchar(50),
-                            postalCode int)",
+                            postalCode int,
+                            PRIMARY KEY (customerID))",
                             
                             "CREATE TABLE Orders
-                            (orderID int PRIMARY KEY,
+                            (orderID int NOT NULL AUTO_INCREMENT,
                             customerID int,
                             FOREIGN KEY (customerID) REFERENCES Customer(customerID),
                             orderDate date,
-                            totalPrice decimal(10, 2))",
+                            totalPrice decimal(10, 2),
+                            PRIMARY KEY(orderID))",
 
                             "CREATE TABLE Author
-                            (authorID int PRIMARY KEY,
+                            (authorID int NOT NULL AUTO_INCREMENT,
                             firstName varchar(50),
                             lastName varchar(50),
-                            descr varchar(50))",
+                            descr varchar(50),
+                            PRIMARY KEY (authorID))",
 
                             "CREATE TABLE Genre
-                            (genreID int PRIMARY KEY,
-                            genreName varchar(20))",
+                            (genreID int NOT NULL AUTO_INCREMENT,
+                            genreName varchar(20),
+                            PRIMARY KEY (genreID))",
 
                             "CREATE TABLE Book
-                            (bookID int PRIMARY KEY,
+                            (bookID int NOT NULL AUTO_INCREMENT,
                             title varchar(250),
                             descr varchar(250),
                             price decimal(10, 2),
@@ -76,10 +83,11 @@
                             genreID int,
                             FOREIGN KEY (genreID) REFERENCES Genre(genreID),
                             isbn varchar(30),
-                            stock int)",
+                            stock int,
+                            PRIMARY KEY (bookID))",
 
                             "CREATE TABLE OrderDetails
-                            (orderDetailsID int PRIMARY KEY,
+                            (orderDetailsID int NOT NULL AUTO_INCREMENT,
                             orderID int,
                             FOREIGN KEY (orderID) REFERENCES Orders(orderID),
                             bookstoreID int,
@@ -87,7 +95,8 @@
                             quantity int,
                             orderType varchar(30),
                             bookID int,
-                            FOREIGN KEY (bookID) REFERENCES Book(bookID))"
+                            FOREIGN KEY (bookID) REFERENCES Book(bookID),
+                            PRIMARY KEY (orderDetailsID))"
                             );
             
                 foreach ($table as &$value){
@@ -95,6 +104,7 @@
                         if (mysqli_query($conn, $value))
                             {
                             echo "Table created\n";
+                            $create_data = true;
                             }
                     }
                     //catch the exception of creating a table
@@ -111,5 +121,14 @@
         }
 
         //create test data as needed.
+        if($create_data)
+            insert_dummy_data($conn);
+        
+    }
+
+    function insert_dummy_data($conn){
+        //echo "insert_dummy_data called";
+        //$sql_insert_customer = "insert into Customer";
+
     }
 ?>
