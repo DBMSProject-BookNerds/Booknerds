24ccb332540287bb84c53bb80b6cf3657e351699
kaufmantw <kaufmantw@jacks.sfasu.edu>
2023-12-08 23:35:37 -0600

    form pages
    created forms

---
+++ forms.php
@@ -0,0 +1,10 @@
+<div class="text-box">
+    <form action='generate_forms.php' method="post">
+            <div>Customers: <input class="hero-btn" id="search-button" type="submit" value="Generate Report" name="customer"/></div>
+            <div>Bookstores: <input class="hero-btn" id="search-button" type="submit" value="Generate Report" name="bookstore"/></div>
+            <div>Books: <input class="hero-btn" id="search-button" type="submit" value="Generate Report" name="book"/></div>
+            <div>Author: <input class="hero-btn" id="search-button" type="submit" value="Generate Report" name="author"/></div>
+            <div>Genre: <input class="hero-btn" id="search-button" type="submit" value="Generate Report" name="genre"/></div>
+            <div>Order: <input class="hero-btn" id="search-button" type="submit" value="Generate Report" name="order"/></div>
+    </form>
+</div>
