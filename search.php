eaef9f6775b5a197bc5d6a444184c9bc2a703a95
kaufmantw <kaufmantw@jacks.sfasu.edu>
2023-12-09 20:22:16 -0600

    The Last Commit
    (before changing test,test)

--- search.php
+++ search.php
@@ -1,59 +1,5 @@
-<?php include '_layout.php';
-if (!empty($_POST)) {
-    $conn = mysqli_connect("localhost",  "root", "default", "");
-    mysqli_select_db($conn, 'BookNerds');
-    $query = search();
-    $result= mysqli_query($conn, $query);
-    echo "<div id=\"centered-div\"><h1>Results</h1></div>
-    <div id=\"centered-div\">
-        <table border='1'>
-        <tr>
-            <th> BookID </th>
-            <th> Title </th>
-            <th> Author First Name </th>
-            <th> Author Last Name </th>
-            <th> Genre </th>
-            <th> ISBN </th>
-        </tr>";
-    while($row = mysqli_fetch_array($result))
-    {
-    echo "<tr>";
-    echo "<td>" . $row["bookID"] . "</td>";
-    echo "<td>" . $row["title"] . "</td>";
-    echo "<td>" . $row["FirstName"] . "</td>";
-    echo "<td>" . $row["LastName"] . "</td>";
-    echo "<td>" . $row["Genre"] . "</td>";
-    echo "<td>" . $row["ISBN"] . "</td>";
-    echo "</tr>";
-    }
-    echo "</table></div>";
-    mysqli_close($conn);
-}
-else {
-}
+<?php include '_layout.php';?>
 
-function search(){
-    $value = 'select bookID, title, firstName as FirstName, lastName as LastName, genreName as Genre, ISBN from book
-              inner join author
-              on book.authorID = author.authorID
-              inner join genre
-              on book.genreID = genre.genreID
-              where(';
-
-    $keys = array_keys(array_filter($_POST));
-    for($x = 0; $x < count($keys); $x++){
-        if (!empty($_POST[$keys[$x]])) {
-            $value = $value . $keys[$x] . ' = ' . '\'' . $_POST[$keys[$x]] . '\'';
-            if ($x != count($keys)-1) {
-                $value = $value . ' and ';
-            }
-        }
-        
-    }
-    $value = $value . ');';
-    return $value;
-}
-?>
 <div id="centered-div">
 	<div class="text-box"> 
 		<!--This form queries the database, specifically the book table-->
@@ -73,7 +19,8 @@ function search(){
 			<div id="label-input"> 
 			<label for="genre-select">Genre:</label>
 			<select id="genre-select" name="genreName"/>
-				<option value="Mystery">Mystery</option>
+			    <option value="">Select genre</option>	
+                <option value="Mystery">Mystery</option>
 				<option value="Comedy">Comedy</option>
 				<option value="Horror">Horror</option>
 				<option value="Romance">Romance</option>
@@ -89,3 +36,68 @@ function search(){
 		<!--<input class="hero-btn" id="search-button" type="submit" value="Search"/>-->
 	</div>
 </div>
+
+<?php 
+    if(!empty($_POST)) {
+        $conn = mysqli_connect("localhost",  "root", "default", "");
+        mysqli_select_db($conn, 'BookNerds');
+        $query = search();
+        $result= mysqli_query($conn, $query);
+        echo "<div id=\"centered-div\"><h1>Results</h1></div>
+        <div id=\"centered-div\">
+            <table border='1'>
+            <tr>
+                <th> BookID </th>
+                <th> Title </th>
+                <th> Author First Name </th>
+                <th> Author Last Name </th>
+                <th> Genre </th>
+                <th> ISBN </th>
+            </tr>";
+        while($row = mysqli_fetch_array($result))
+        {
+        echo "<tr>";
+        echo "<td>" . $row["bookID"] . "</td>";
+        echo "<td>" . $row["title"] . "</td>";
+        echo "<td>" . $row["FirstName"] . "</td>";
+        echo "<td>" . $row["LastName"] . "</td>";
+        echo "<td>" . $row["Genre"] . "</td>";
+        echo "<td>" . $row["ISBN"] . "</td>";
+        echo "</tr>";
+        }
+        echo "</table></div>";
+        mysqli_close($conn);
+    }
+
+function search(){
+    $value = 'select bookID, title, firstName as FirstName, lastName as LastName, genreName as Genre, ISBN from book
+              inner join author
+              on book.authorID = author.authorID
+              inner join genre
+              on book.genreID = genre.genreID
+              ';
+
+    $condtion_empty = true;
+
+    $keys = array_keys(array_filter($_POST));
+    $temp = "";
+    for($x = 0; $x < count($keys); $x++){
+        if (!empty($_POST[$keys[$x]]) && $_POST[$keys[$x]] != null){
+            $condtion_empty = false;
+            $temp = $temp . $keys[$x] . ' = ' . '\'' . $_POST[$keys[$x]] . '\'';
+            if ($x != count($keys)-1) {
+                $temp = $temp . ' and ';
+            }
+        }      
+    }
+
+    if (!$condtion_empty){
+        $value = $value . 'where('. $temp . ');';
+    }else{
+        $value = $value . ';';
+    }
+
+    return $value;
+}
+?>
+
